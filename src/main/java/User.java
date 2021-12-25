8b7eac5d4bea5c3b4464da911f5348f14024aba4
aehrlich1 <anatol.ehrlich@gmail.com>
2021-12-25 12:21:29 +0100

    added lombok dependency

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,12 @@
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
   private String firstName;
   private String lastName;
